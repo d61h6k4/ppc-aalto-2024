d1e18d00d7721cc2ece97c4ecd148f880272a87a
d61h6k4 <ddbihbka@gmail.com>
2024-04-21 13:14:24 +0200

    Add a streaming mean calculation
    IMPORTANT: slows down twice

--- exercises/pre0/pre0.cc
+++ exercises/pre0/pre0.cc
@@ -4,19 +4,19 @@
 Result calculate(int ny, int nx, const float *data, int y0, int x0, int y1,
                  int x1) {
   std::array<float, 3> avg = {0.0f, 0.0f, 0.0f};
-  std::array<double, 3> avg_d = {0.0, 0.0, 0.0};
-  std::array<int, 3> num = {0, 0, 0};
+  std::array<float, 3> sum = {0.0f, 0.0f, 0.0f};
 
-  for (auto c = 0; c < 3; ++c) {
-    for (auto y = y0; y < y1; ++y) {
-      for (auto x = x0; x < x1; ++x) {
-        avg_d[c] += static_cast<double>(data[c + 3 * x + 3 * nx * y]);
-        num[c] += 1;
+  for (auto y = y0; y < y1; ++y) {
+    for (auto x = x0; x < x1; ++x) {
+      for (auto c = 0; c < 3; ++c) {
+          float q = data[c + 3 * x + 3 * nx * y] - avg[c];
+          float tmp_sum = sum[c] + 1.0f;
+          float r = q / tmp_sum;
+
+          avg[c] += r;
+          sum[c] = tmp_sum;
       }
     }
-  }
-  for (auto c = 0; c < 3; ++c) {
-    avg[c] = static_cast<float>(avg_d[c] / static_cast<double>(num[c]));
   }
 
   return Result{.avg = avg};
